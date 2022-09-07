4391f9a7a60b106498e98fc11ed7ce5833dc022a
Andrey <oleynik.andrey01@gmail.com>
2022-09-07 20:46:00 +0300

    style: added icons for sent message status

---
+++ src/Components/Dialog/Icon/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import * as icons from "../../../icons";
+
+const Icon = ({ size = 20, name, className = "" }) => {
+  const [width, height] = size instanceof Array ? size : [size, size];
+  const [icon, viewBox] = icons[name];
+  // console.log(name)
+  return (
+    <svg
+      className={"icon " + className}
+      width={width}
+      height={height}
+      dangerouslySetInnerHTML={{ __html: icon }}
+      viewBox={viewBox}
+    />
+  );
+};
+
+export default Icon;
