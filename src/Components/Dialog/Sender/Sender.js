762b63d0cb01dcbca202637402d421a5e8f4a310
OlAndrey <oleynik.andrey01@gmail.com>
2023-05-16 22:02:54 +0300

    chore(Dialog): added the ability to insert emoji in messages

--- src/Components/Dialog/Sender/Sender.js
+++ src/Components/Dialog/Sender/Sender.js
@@ -1,33 +1,37 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import "./styles.css";
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import InputEmoji from 'react-input-emoji'
+import './styles.css'
 
 const Sender = ({ onAddMessage, onCreateDialog, dialog, uid, youId }) => {
-  const [value, setValue] = useState("");
-  let navigate = useNavigate();
-  const onChange = (event) => setValue(event.target.value);
+  const [text, setText] = useState('')
+  let navigate = useNavigate()
+
+  const onChange = (text) => setText(text)
   const onSubmit = (event) => {
-    event.preventDefault();
-    if(dialog.lastMessage)
-      onAddMessage(dialog.path, value, dialog.unreadedMessages + 1, uid);
-    else
-      onCreateDialog(value, uid, youId).then(id => navigate("../dialog/" + id, { replace: true }))
-    setValue("");
-  };
+    event.preventDefault()
+    if (text.trim() !== 0) {
+      if (dialog.lastMessage)
+        onAddMessage(dialog.path, text, dialog.unreadedMessages + 1, uid)
+      else
+        onCreateDialog(text, uid, youId).then((id) =>
+          navigate('../dialog/' + id, { replace: true })
+        )
+        setText('')
+    }
+  }
 
   return (
     <form className="sender" onSubmit={onSubmit}>
-      <input
+      <InputEmoji
         placeholder="Enter message"
-        value={value}
+        value={text}
         onChange={onChange}
-        required
       />
-      
-      <button>Send</button>
+
+      <button type="submit">Send</button>
     </form>
-  );
-};
+  )
+}
 
-
-export default Sender;
+export default Sender
