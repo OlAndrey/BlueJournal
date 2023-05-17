de7fb701976b327dad927d5b3c297126f309885e
OlAndrey <oleynik.andrey01@gmail.com>
2023-05-17 16:40:59 +0300

    chore(Dialog): added error handling

--- src/Components/Dialog/Dialog.js
+++ src/Components/Dialog/Dialog.js
@@ -1,97 +1,124 @@
-import React, { memo, useContext, useEffect, useMemo, useRef } from "react";
+import React, { memo, useContext, useEffect, useMemo, useRef } from 'react'
 
-import Item from "./Item/DialogItem";
-import Title from "./Title/DialogTitle";
+import Item from './Item/DialogItem'
+import Title from './Title/DialogTitle'
 
-import { normalizeDialog } from "./helpers";
+import { normalizeDialog } from './helpers'
 
-import "./styles.css";
-import { deleteMessage, updateMessageStatus } from "../../API/dialogAPI";
-import { useCollectionData } from "react-firebase-hooks/firestore";
-import { Context } from "../..";
-import PreLoader from "../PreLoader/PreLoader";
+import './styles.css'
+import { deleteMessage, updateMessageStatus } from '../../API/dialogAPI'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+import { Context } from '../../index'
+import PreLoader from '../PreLoader/PreLoader'
 
 const Dialog = ({ dialog, me, you }) => {
-  const dialogRef = useRef(false);
-  const messageLength = useRef(0);
-  const { firestore}  = useContext(Context);
-  const [ messages, loading ] = useCollectionData(
-    firestore.collection(dialog.path + '/message').orderBy("id")
+  const dialogRef = useRef(false)
+  const messageLength = useRef(0)
+  const { firestore } = useContext(Context)
+  const [messages, loading] = useCollectionData(
+    firestore.collection(dialog.path + '/message').orderBy('id')
   )
 
-  messageLength.current = (messages) ? messages.length : 0;
+  messageLength.current = messages ? messages.length : 0
 
   const scrollHandler = () => {
-    if(messages){
-      let links = document.querySelectorAll('.unreaded');
-      try{
-        var wt = document.querySelector(".overflow").scrollTop; 
-        var wh = document.querySelector(".overflow").offsetHeight;
-      } catch{
+    if (messages) {
+      let links = document.querySelectorAll('.unreaded')
+      try {
+        var wt = document.querySelector('.overflow').scrollTop
+        var wh = document.querySelector('.overflow').offsetHeight
+      } catch {
         console.error("Don't find dialog block")
-      }  
+      }
       for (let i = 0; i < links.length; i++) {
-        var eh = links[i].offsetHeight;  
-        var et = links[i].offsetTop;
-        if (wt + wh >= et && wt + wh - eh * 2 <= et + (wh - eh)){
-          const id = links[i].dataset.target;
+        var eh = links[i].offsetHeight
+        var et = links[i].offsetTop
+        if (wt + wh >= et && wt + wh - eh * 2 <= et + (wh - eh)) {
+          const id = links[i].dataset.target
           for (let j = 0; j < messages.length; j++) {
             if (String(messages[j].id) === id) {
-              console.log(id)
               updateMessageStatus(messages[j].path)
-            } 
+            }
           }
         }
       }
     }
-  };
-  
-  useEffect(() => {  
-    const overflow = document.querySelector(".overflow")
-    overflow.removeEventListener('scroll', scrollHandler);
-    overflow.scrollTop = overflow.scrollHeight;
-    overflow.addEventListener('scroll', scrollHandler);
+  }
+
+  useEffect(() => {
+    const overflow = document.querySelector('.overflow')
+    overflow.removeEventListener('scroll', scrollHandler)
+    overflow.scrollTop = overflow.scrollHeight
+    overflow.addEventListener('scroll', scrollHandler)
     scrollHandler()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [messageLength.current]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [messageLength.current])
 
+  const normalizedDialog = normalizeDialog(messages, me.uid)
 
-  const normalizedDialog = normalizeDialog(messages, me.uid);
-
-  const value = useMemo(() => {
-      if(!dialogRef.current){
-        dialogRef.current = normalizedDialog;
-        return normalizedDialog;
+  const dialogValue = useMemo(() => {
+    try {
+      if (!dialogRef.current) {
+        dialogRef.current = normalizedDialog
+        return normalizedDialog
       }
 
-      const dialogLength = dialogRef.current.length - 1;
+      const dialogLength = dialogRef.current.length - 1
       const normalizedLength = normalizedDialog.length
-      if(
-        (dialogRef.current.length === normalizedLength + 2) ||
-        ((dialogRef.current.length === normalizedLength + 1) && !normalizedDialog[normalizedLength-1].isReverse)){
-          const newLastMessages = dialogRef.current[dialogLength].messages.map(item => {return {...item, status: "readed"}})
-          return dialogRef.current.slice(0, dialogLength).concat({...dialogRef.current[dialogLength], messages: newLastMessages})
+      if (
+        dialogRef.current.length === normalizedLength + 2 ||
+        (dialogRef.current.length === normalizedLength + 1 &&
+          !normalizedDialog[normalizedLength - 1].isReverse)
+      ) {
+        const newLastMessages = dialogRef.current[dialogLength].messages.map(
+          (item) => {
+            return { ...item, status: 'readed' }
+          }
+        )
+        return dialogRef.current.slice(0, dialogLength).concat({
+          ...dialogRef.current[dialogLength],
+          messages: newLastMessages
+        })
       }
 
-      dialogRef.current = normalizedDialog;
+      dialogRef.current = normalizedDialog
       return normalizedDialog
-    }, [normalizedDialog])
+    } catch (error) {
+      console.error(error)
+      return { error: 'Something went wrong!' }
+    }
+  }, [normalizedDialog])
 
-  if(loading)
-    return <PreLoader />  
+  if (loading) return <PreLoader />
 
   return (
     <>
-        {value.map((item) =>
-          item.type === "message" ? (
-            <Item {...item} me={me} you={you} key={item.id} onDeleteMessage={deleteMessage} unreadedMessages={dialog.unreadedMessages} />
+      {Array.isArray(dialogValue) ? (
+        dialogValue.map((item) =>
+          item.type === 'message' ? (
+            <Item
+              {...item}
+              me={me}
+              you={you}
+              key={item.id}
+              onDeleteMessage={deleteMessage}
+              unreadedMessages={dialog.unreadedMessages}
+            />
           ) : (
             <Title key={item.id} date={item.date} type={item.type} />
           )
-        )}
+        )
+      ) : (
+        <div className="h-75 d-flex justify-content-center">
+          <div className="d-flex justify-content-center flex-column align-items-center">
+            <h3>{dialogValue.error}</h3>
+            <p>Please reload the page.</p>
+          </div>
+        </div>
+      )}
     </>
-  );
-};
+  )
+}
 
-const DialogContainer = memo(Dialog);
-export default DialogContainer;
+const DialogContainer = memo(Dialog)
+export default DialogContainer
--- src/Components/Dialog/helpers.js
+++ src/Components/Dialog/helpers.js
@@ -1,71 +1,85 @@
-import dayjs from "dayjs";
+import dayjs from 'dayjs'
 
 export const normalizeDialog = (dialog, userId) => {
-  let wasTitleStatus = false;
-  let wasStatusSended = false;
-  const newDialog = [];
+  try {
+    let wasTitleStatus = false
+    let wasStatusSended = false
+    const newDialog = []
 
-  if(!dialog)
-    return newDialog;
+    if (!dialog) return newDialog
 
-  dialog.forEach((item, i) => {
-    if (i === 0 || dialog[i - 1]) {
-      const first = dayjs((i === 0 ? item : dialog[i - 1]).date);
-      const second = dayjs(item.date);
-      const diff = second.$W - first.$W;
-      
-      if (i === 0 || diff) {
-        newDialog.push({
-          type: "title",
-          id: `item-title-${item.id}`,
-          date: item.date,
-        });
+    dialog.forEach((item, i) => {
+      if (i === 0 || dialog[i - 1]) {
+        const first = dayjs((i === 0 ? item : dialog[i - 1]).date)
+        const second = dayjs(item.date)
+        const diff = second.$W - first.$W
+
+        if (i === 0 || diff) {
+          newDialog.push({
+            type: 'title',
+            id: `item-title-${item.id}`,
+            date: item.date
+          })
+        }
+        if (
+          item.status === 'sended' &&
+          !wasTitleStatus &&
+          item.is !== userId &&
+          !wasStatusSended
+        ) {
+          wasTitleStatus = true
+          wasStatusSended = true
+          newDialog.push({
+            type: 'status',
+            id: `title-status-${item.id}`,
+            date: item.date
+          })
+        }
       }
-      if (item.status === "sended" && !wasTitleStatus && item.is !== userId && !wasStatusSended){
-        wasTitleStatus = true
-        wasStatusSended = true;
-        newDialog.push({
-          type: "status",
-          id: `title-status-${item.id}`,
-          date: item.date,
-        });
-      }
-    }
 
-    if (i === 0 || item.is !== dialog[i - 1].is || wasStatusSended) {
-      wasStatusSended = false;
-      newDialog.push({
-        type: "message",
-        id: `item-message-${item.id}`,
-        isReverse: item.is === userId,
-        isRemovable: item.is === userId,
-        messages: [
-          {
+      if (
+        i === 0 ||
+        item.is !== dialog[i - 1].is ||
+        wasStatusSended ||
+        newDialog[newDialog.length - 1].type !== 'message'
+      ) {
+        wasStatusSended = false
+        newDialog.push({
+          type: 'message',
+          id: `item-message-${item.id}`,
+          isReverse: item.is === userId,
+          isRemovable: item.is === userId,
+          messages: [
+            {
+              text: item.message,
+              id: item.id,
+              date: item.date,
+              status: item.status,
+              path: item.path,
+              isDeleted: item.isDeleted
+            }
+          ]
+        })
+      } else {
+        const position = newDialog.length - 1
+
+        newDialog[position] = {
+          ...newDialog[position],
+          messages: newDialog[position].messages.concat({
             text: item.message,
+            status: item.status,
             id: item.id,
             date: item.date,
-            status: item.status,
             path: item.path,
             isDeleted: item.isDeleted
-          },
-        ],
-      });
-    } else {
-      const position = newDialog.length - 1;
+          })
+        }
+      }
+    })
 
-      newDialog[position] = {
-        ...newDialog[position],
-        messages: newDialog[position].messages.concat({
-          text: item.message,
-          status: item.status,
-          id: item.id,
-          date: item.date,
-          path: item.path,
-          isDeleted: item.isDeleted
-        }),
-      };
-    }
-  });
-
-  return newDialog;
-};
+    return newDialog
+  } catch (error) {
+    console.error(error);
+    return {error: "Something went wrong!"}
+  }
+}
