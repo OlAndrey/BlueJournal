d341616974e1bd6f7ac413c55b8bd8b555c7a77f
OlAndrey <oleynik.andrey01@gmail.com>
2023-05-27 00:00:59 +0300

    feat: added firebase-functions

--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,34 @@
+
+const functions = require("firebase-functions");
+const logger = require("firebase-functions/logger");
+
 const express = require('express')
+const cors = require('cors')
+var admin = require("firebase-admin");
 
+const getNews = async (req, res) => {
+  try {
+    res.status(200).json({
+      message: "I'm working!"
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Error'
+    })
+  }
+}
 
-const server = express()
+const app = express();
+app.use(
+  cors()
+)
+const port = process.env.PORT || 3500
 
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-// Server Setup
-const port = process.env.PORT || 8000
-server.listen(port, () => {
-    console.log(`Server listening on: ${port}`)
-})
+app.get('/api/test', getNews)
+app.get('/*', (req, res) => res.json({message: 'error url'}))
+
+exports.app = functions.https.onRequest(app)
